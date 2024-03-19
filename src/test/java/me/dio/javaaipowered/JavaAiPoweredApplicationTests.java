ae76eb4b189cee7f7e31dc840c4531003cacfd4c
José E A Santos <jose.e.a.santos@gmail.com>
2024-03-19 20:28:22 -0300

    initial commit

---
+++ src/test/java/me/dio/javaaipowered/JavaAiPoweredApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.javaaipowered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAiPoweredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
